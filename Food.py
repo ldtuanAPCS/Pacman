64a21cebe4e608ffc019caf4a32d4e01bf006467
ldtuanAPCS <ldtuan@apcs.vn>
2019-03-18 00:34:20 +0700

    17/3 midnight

--- Food.py
+++ Food.py
@@ -1,9 +1,6 @@
 class Food(object):
     def __init__(self, x, y):
         self.location = x, y
-    
-    def location(self):
-        return self.location
 
     def __repr__(self):
         st = "Food location: ", self.location[0], ", ", self.location[1] 
